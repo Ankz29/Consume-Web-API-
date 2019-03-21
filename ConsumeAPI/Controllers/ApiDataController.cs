87167e4235ef8ff4e75f0d290c813ebbda112e37
Ankita <ankita.m@softwaysolutions.com>
2019-03-21 13:16:48 +0530

    POC on Consuming the RESTfull Web API

---
+++ ConsumeAPI/Controllers/ApiDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Web.Mvc;
+
+
+
+namespace ConsumeAPI.Controllers
+{
+    public class ApiDataController : Controller
+    {
+        //
+        // GET: /ApiData/
+
+        public PartialViewResult Index(int Zip)
+        {
+            string resultant = " ";
+            {
+                string ApiURL = "http://localhost:56212/api/ValuesDealers/Get?Zip="+ Zip;
+                WebRequest request = WebRequest.Create(ApiURL);
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                resultant = reader.ReadLine();
+                //while(str != null)  
+                //{  
+                //  Console.WriteLine(str);  
+                //  str = reader.ReadLine();  
+                //}  
+            }
+            return PartialView();
+        }
+
+    }
+}
+
+
+
